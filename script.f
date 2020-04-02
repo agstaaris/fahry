6a7205ea769a3786d64fcbff777417216d22370a
fahry-ID <63068221+fahry-ID@users.noreply.github.com>
2020-04-03 01:18:04 +0700

    Add files via upload

---
+++ script.f
@@ -0,0 +1,21 @@
+import OS
+
+os system (*8.8.8.8*)
+
+a = 0
+
+while (a < 3):
+    print (*welcome*)
+    print (*WELCOME*)
+    
+    Sebuah alat untuk spoon radio
+
+[1] Cek detail akun spoon
+[2] Cek akun user lain
+[3] Tambah fans
+[4] Bom listener
+[5] Bom tap love
+[6] Tarik bot
+[7] Bot listener + Tap love
+
+Bebas asal sopan
